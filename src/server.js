18cfe12c55007717905588b02b81e499010bde0d
Andrew Liu <andrew.a.liu.19@dartmouth.edu>
2018-06-01 21:42:48 -0400

    testing

--- src/server.js
+++ src/server.js
@@ -59,7 +59,6 @@ const slackbot = controller.spawn({
   if (err) { throw new Error(err); }
 });
 
-console.log('hi');
 // prepare webhook
 // for now we won't use this but feel free to look up slack webhooks
 controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
@@ -68,7 +67,6 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   });
 });
 
-console.log('hi1');
 
 // example hello response
 controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
@@ -81,90 +79,80 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
   });
 });
 
-console.log('hi2');
-
-// example hello response
-controller.hears(['hungry', 'food', 'restaurant'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-  bot.startConversation(message, (err, convo) => {
-    convo.ask({
-      text: 'Do you want restaurant recommendations near you?',
-    }, [
-      {
-        pattern: bot.utterances.yes,
-        callback(reply) {
-          convo.ask(
-            { text: 'Great! What type of food would you like?' },
-            [
-              {
-                default: true,
-                callback(termMessage) {
-                  convo.ask({ text: 'Where are you right now? ' }, [
-                    {
-                      default: true,
-                      callback(locationMessage) {
-                        yelpClient.search({ term: termMessage.text, location: locationMessage.text }).then((restaurants) => {
-                          bot.reply(
-                            locationMessage,
-                            {
-                              text: 'Here is a restaurant in the area',
-                              attachments: [
-                                {
-                                  title: `${restaurants.jsonBody.businesses[0].name}`,
-                                  title_link: `${restaurants.jsonBody.businesses[0].url}`,
-                                  text: `Rating: ${restaurants.jsonBody.businesses[0].rating} stars`,
-                                  image_url: `${restaurants.jsonBody.businesses[0].image_url}`,
-                                },
-                              ],
-                            },
-                          );
-                        }).catch((error) => {
-                          convo.say('No restaurants found. Sorry.');
-                        });
-                      },
-                    },
-                  ]);
-                  convo.next();
-                },
-              },
-            ],
-          );
-          convo.next();
-        },
-      },
-      {
-        pattern: bot.utterances.no,
-        callback(reply) {
-          convo.say('Bad choice.');
-          convo.next();
-        },
-      },
-      {
-        default: true,
-        callback(reply) {
-          // do nothing
-        },
-      },
-    ]);
-  });
-});
-
-console.log('hi3');
+// // example hello response
+// controller.hears(['hungry', 'food', 'restaurant'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+//   bot.startConversation(message, (err, convo) => {
+//     convo.ask({
+//       text: 'Do you want restaurant recommendations near you?',
+//     }, [
+//       {
+//         pattern: bot.utterances.yes,
+//         callback(reply) {
+//           convo.ask(
+//             { text: 'Great! What type of food would you like?' },
+//             [
+//               {
+//                 default: true,
+//                 callback(termMessage) {
+//                   convo.ask({ text: 'Where are you right now? ' }, [
+//                     {
+//                       default: true,
+//                       callback(locationMessage) {
+//                         yelpClient.search({ term: termMessage.text, location: locationMessage.text }).then((restaurants) => {
+//                           bot.reply(
+//                             locationMessage,
+//                             {
+//                               text: 'Here is a restaurant in the area',
+//                               attachments: [
+//                                 {
+//                                   title: `${restaurants.jsonBody.businesses[0].name}`,
+//                                   title_link: `${restaurants.jsonBody.businesses[0].url}`,
+//                                   text: `Rating: ${restaurants.jsonBody.businesses[0].rating} stars`,
+//                                   image_url: `${restaurants.jsonBody.businesses[0].image_url}`,
+//                                 },
+//                               ],
+//                             },
+//                           );
+//                         }).catch((error) => {
+//                           convo.say('No restaurants found. Sorry.');
+//                         });
+//                       },
+//                     },
+//                   ]);
+//                   convo.next();
+//                 },
+//               },
+//             ],
+//           );
+//           convo.next();
+//         },
+//       },
+//       {
+//         pattern: bot.utterances.no,
+//         callback(reply) {
+//           convo.say('Bad choice.');
+//           convo.next();
+//         },
+//       },
+//       {
+//         default: true,
+//         callback(reply) {
+//           // do nothing
+//         },
+//       },
+//     ]);
+//   });
+// });
 
 controller.hears('help', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.reply(message, 'Hi! If you say hi to me, I will say hi back to you. Otherwise, to get a restaurant recommendation, tell me you are hungry!');
 });
-
-console.log('hi4');
 
 // catch-all response
 controller.hears('^.*', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.reply(message, 'Sorry, I do not understand what you are saying.');
 });
 
-console.log('hi5');
-
 controller.on('outgoing_webhook', (bot, message) => {
   bot.replyPublic(message, 'yeah yeah');
 });
-
-console.log('hi6');
